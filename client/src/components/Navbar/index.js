a072a3b350b482dd94bc206c05a5e7d353aacb81
Richard Ho <richardpanda1993@gmail.com>
2017-10-26 20:48:44 -0700

    Add home to navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -6,6 +6,7 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="Navbar-nav">
+        <a className="Navbar-home" href="/">Composition</a>
         <ul className="Navbar-list">
           <li className="Navbar-item"><a className="Navbar-link" href="/signin">Sign In</a></li>
           <li className="Navbar-item"><a className="Navbar-link" href="/signup">Sign Up</a></li>
