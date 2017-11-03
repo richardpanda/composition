e5edafb9d74b10ef52f394d338ecd88932376880
Richard Ho <richardpanda1993@gmail.com>
2017-11-02 21:05:32 -0700

    Remove handlers and utils

--- server/api/middleware/middleware.go
+++ server/api/middleware/middleware.go
@@ -1,15 +1,11 @@
 package middleware
 
 import (
-	"context"
 	"database/sql"
-	"net/http"
-	"strings"
 
 	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"github.com/richardpanda/composition/server/api/types"
-	"github.com/richardpanda/composition/server/api/utils"
 )
 
 func DB(db *sql.DB) gin.HandlerFunc {
@@ -42,33 +38,4 @@ func Authenticate() gin.HandlerFunc {
 		c.Set("user", t.Claims.(jwt.MapClaims))
 		c.Next()
 	}
-}
-
-func IsAuthenticated(w http.ResponseWriter, r *http.Request) *http.Request {
-	authHeader := r.Header.Get("Authorization")
-
-	if authHeader == "" {
-		utils.SetErrorResponse(w, 401, "Authorization header is required.")
-		return nil
-	}
-
-	if !strings.HasPrefix(authHeader, "Bearer ") {
-		utils.SetErrorResponse(w, 400, "Malformed authorization header.")
-		return nil
-	}
-
-	tokenString := authHeader[len("Bearer "):]
-
-	t, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		return types.JWTSecret, nil
-	})
-
-	if err != nil || !t.Valid {
-		utils.SetErrorResponse(w, 400, "Invalid token.")
-		return nil
-	}
-
-	ctx := r.Context()
-	ctx = context.WithValue(ctx, "user", t.Claims.(jwt.MapClaims))
-	return r.WithContext(ctx)
 }
