e4f177366add4852caaae92cce1281f6c103309d
Unknown <richardpanda1993@gmail.com>
2017-11-11 14:26:53 -0800

    Add dropdown to Navbar

--- client/src/containers/Navbar/index.js
+++ client/src/containers/Navbar/index.js
@@ -4,19 +4,32 @@ import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
   render() {
-    const { isLoggedIn } = this.props;
+    const { isLoggedIn, username } = this.props;
 
     return (
-      <nav className="navbar navbar-dark bg-dark">
+      <nav className="navbar navbar-dark navbar-expand bg-dark">
         <Link to="/"><div className="navbar-brand">Composition</div></Link>
-        {isLoggedIn ? (
-          <Link to="/signout"><button className="btn btn-primary">Sign Out</button></Link>
-        ) : (
-          <div>
-            <Link to="/signin"><button className="btn btn-primary mr-2">Sign In</button></Link>
-            <Link to="/signup"><button className="btn btn-light">Sign Up</button></Link>
-          </div>
-        )}
+        <div className="ml-auto">
+          {isLoggedIn ? (
+            <div className="navbar px-0 py-0">
+              <ul className="navbar-nav">
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle text-primary" href="#" data-toggle="dropdown">
+                    {username}
+                  </a>
+                  <div className="dropdown-menu dropdown-menu-right">
+                    <Link className="dropdown-item" to="/signout">Sign Out</Link>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          ) : (
+            <div>
+              <Link to="/signin"><button className="btn btn-link mr-2">Sign In</button></Link>
+              <Link to="/signup"><button className="btn btn-light">Sign Up</button></Link>
+            </div>
+          )}
+        </div>
       </nav>
     );
   }
@@ -24,6 +37,7 @@ class Navbar extends Component {
 
 const mapStateToProps = state => ({
   isLoggedIn: state.auth.isLoggedIn,
+  username: state.auth.username,
 });
 
 export default connect(mapStateToProps)(Navbar);
