e3cc0cb7df976995895c05a30cd273c6e238bd11
Unknown <richardpanda1993@gmail.com>
2017-11-10 19:54:48 -0800

    Fix bug that sent two requests for article previews

--- client/src/containers/ArticlePreviews/index.js
+++ client/src/containers/ArticlePreviews/index.js
@@ -2,24 +2,32 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+const parsePageNumber = (queryString) => {
+  const pageRegex = /page=(\d+)/i;
+  return (queryString.match(pageRegex) && parseInt(pageRegex.exec(queryString)[1], 10)) || 1;
+};
+
 class ArticlePreviews extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       articlePreviews: [],
+      page: parsePageNumber(window.location.search),
       error: ''
     };
     this.fetchArticlePreviews = this.fetchArticlePreviews.bind(this);
   }
 
   componentDidMount() {
-    this.fetchArticlePreviews(this.props.page);
+    this.fetchArticlePreviews(this.state.page);
   }
 
-  componentWillUpdate(nextProps, nextState) {
-    if (this.props.page !== nextProps.page) {
-      this.fetchArticlePreviews(nextProps.page);
+  componentDidUpdate() {
+    const { page } = this.props;
+    if (this.state.page !== page) {
+      this.setState({ page });
+      this.fetchArticlePreviews(page);
     }
   }
 
@@ -39,8 +47,7 @@ class ArticlePreviews extends Component {
   }
 
   render() {
-    const { articlePreviews, error } = this.state;
-    const { page } = this.props;
+    const { articlePreviews, page, error } = this.state;
 
     const showPrevious = page > 1;
     const showNext = articlePreviews.length === 10;
@@ -57,18 +64,21 @@ class ArticlePreviews extends Component {
           </div>
         ))}
         <div>
-          {showPrevious && <Link to={`?page=${page-1}`}><button className="btn btn-secondary btn-small mr-2">Previous</button></Link>}
-          {showNext && <Link to={`?page=${page+1}`}><button className="btn btn-secondary btn-small">Next</button></Link>}
+          {showPrevious && 
+            <Link to={`?page=${page-1}`}>
+              <button className="btn btn-secondary btn-small mr-2">Previous</button>
+            </Link>
+          }
+          {showNext &&
+            <Link to={`?page=${page+1}`}>
+              <button className="btn btn-secondary btn-small">Next</button>
+            </Link>
+          }
         </div>
       </div>
     );
   }
 }
-
-const parsePageNumber = (queryString) => {
-  const pageRegex = /page=(\d+)/i;
-  return (queryString.match(pageRegex) && parseInt(pageRegex.exec(queryString)[1], 10)) || 1;
-};
 
 const mapStateToProps = state => ({
   page: parsePageNumber(state.router.location.search),
